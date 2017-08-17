12ac28fd7bec117fbd9647d7062e3ae6d2febd8f
kanja mwingirwa <kanjamwingirwa@gmail.com>
2017-08-17 11:27:58 +0300

    subtraction function on calculator

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,11 @@
       var number2 = parseInt(prompt("Enter another number:"));
       var result = add(number1, number2);
       alert(result);
-    
+
+     var subtract = function(number1, number2) {
+       return number2-number1;
+     };
+     var number1 = parseInt(prompt("Enter a number:"));
+     var number2 = parseInt(prompt("Enter another number:"));
+     var result = subtract(number2,number1);
+     alert(result);
